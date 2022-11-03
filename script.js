8ccdf9c0eded356b7beadcf926f84ac3496897e1
mesbahpour <mesbahpour.a@gmail.com>
2022-11-03 22:23:52 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let clr1 = document.getElementById('color1');
+let clr2 = document.getElementById('color2');
+let clr3 = document.getElementById('color3');
+let clr4 = document.getElementById('color4');
+let clr5 = document.getElementById('color5');
+
+
+function changeColor(el) {
+    document.body.style.backgroundColor = el.style.backgroundColor;
+}
